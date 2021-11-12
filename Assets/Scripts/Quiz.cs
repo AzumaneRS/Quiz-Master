f8c237f77e5a163c34b8a37d806b5b4006058136
BorisB <beara.boris@gmail.com>
2021-11-12 21:30:06 +0100

    IIIII dobio sam jebene errore jer mi nesto fali... fuck this...

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Quiz : MonoBehaviour
 {
@@ -24,9 +24,15 @@ public class Quiz : MonoBehaviour
     [SerializeField] Image timerImage;
     Timer timer;
 
+    [Header("Scoring")]
+    [SerializeField]TextMeshProUGUI scoreText;
+    Scorekeeper scoreKeeper;
+
+
     void Start()
     {
         timer = FindObjectOfType<Timer>();
+        scoreKeeper = FindObjectOfType<Scorekeeper>();
     }
 
     void Update()
@@ -51,6 +57,7 @@ public class Quiz : MonoBehaviour
         DisplayAnswer(index);
         SetButtonState(false);
         timer.CancelTimer();
+        scoreText.text = "Score: " + scoreKeeper.CalculateScore() + "%";
     }
 
     void DisplayAnswer(int index)
@@ -62,6 +69,7 @@ public class Quiz : MonoBehaviour
             questionText.text = "Correct";
             buttonImage = answerButtons[index].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
+            scoreKeeper.IncrementCorrectAnswers();
         }
         else
         {
@@ -82,6 +90,7 @@ public class Quiz : MonoBehaviour
             SetDefaultButtonSprite();
             GetRandomQuestion();
             DispalyQuestion();
+            scoreKeeper.IncrementQuestionsSeen();
         }
     }
 
